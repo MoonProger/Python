c1eb00b5da919f534ad7d677d42bde9d20479285
MoonProger <danchenkonadya.dn8@gmail.com>
2024-09-30 03:32:31 +0300

    Добавлен файл с кодом в папку '2 week'

---
+++ 2 week/task6.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def convert(self, s, num_rows):
+        if num_rows == 1 or num_rows >= len(s):
+            return s
+
+        rows = [''] * num_rows
+
+        cur_row = 0
+        down = False
+
+        for char in s:
+            rows[cur_row] += char
+
+            if cur_row == 0 or cur_row == num_rows - 1:
+                down = not down
+
+            if down:
+                cur_row += 1
+            else:
+                cur_row -= 1
+
+        return ''.join(rows)
