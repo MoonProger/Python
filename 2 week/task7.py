e1f3675b8aeadc82dc4756da17c9e8944de2d059
MoonProger <danchenkonadya.dn8@gmail.com>
2024-09-30 03:40:24 +0300

    Добавлен файл с кодом в папку '2 week'

---
+++ 2 week/task7.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        result = []
+
+        cur_stack = [""]
+        opened = [0]
+        closed = [0]
+
+        while cur_stack:
+            cur = cur_stack.pop()
+            open_count = opened.pop()
+            close_count = closed.pop()
+
+            if len(cur) == 2 * n:
+                result.append(cur)
+                continue
+
+
+            if open_count < n:
+                cur_stack.append(cur + "(")
+                opened.append(open_count + 1)
+                closed.append(close_count)
+
+            if close_count < open_count:
+                cur_stack.append(cur + ")")
+                opened.append(open_count)
+                closed.append(close_count + 1)
+
+        return result
